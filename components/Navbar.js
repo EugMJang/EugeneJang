a602afa24c6c20e2d39778a05f5487a3501155c4
Eugene Jang <eug.jang123@gmail.com>
2022-06-23 16:10:05 -0500

    Replace links with prebuilt one given in nextjs example

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,8 +2,7 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from 'next/link';
-import {Link as MUILink} from '@mui/material'
+import Link from './Link';
 import { AppBar, Button, Toolbar } from '@mui/material';
 
 export default function Navbar() {
@@ -13,15 +12,15 @@ export default function Navbar() {
                 <Container maxWidth="xl">
                     <Toolbar sx={{pt: 8, pb: 3}}>
                         <Typography variant='h3' component='h1' sx={{flexGrow: 1}}>
-                            <Link href='/' passHref><MUILink color='inherit' underline='none'>
+                            <Link href='/' passHref color='inherit' underline='none'>
                                 Eugene Jang
-                            </MUILink></Link>
+                            </Link>
                         </Typography>
                         <Box sx={{pt: 2}}>
-                            <Button sx={{px: 2}} color='inherit'><Link href='/about' passHref><MUILink color='inherit' underline='none'>About me</MUILink></Link></Button>
-                            <Button sx={{px: 2}} color='inherit'><Link href='/projects' passHref><MUILink color='inherit' underline='none'>Projects</MUILink></Link></Button>
-                            <Button sx={{px: 2}} color='inherit'><Link href='/resume' passHref><MUILink color='inherit' underline='none'>Resume</MUILink></Link></Button>
-                            <Button sx={{px: 2}} color='inherit'><Link href='/contact' passHref><MUILink color='inherit' underline='none'>Contact me</MUILink></Link></Button>
+                            <Button sx={{px: 2}} color='inherit'><Link href='/about' passHref color='inherit' underline='none'>About me</Link></Button>
+                            <Button sx={{px: 2}} color='inherit'><Link href='/projects' passHref color='inherit' underline='none'>Projects</Link></Button>
+                            <Button sx={{px: 2}} color='inherit'><Link href='/resume' passHref color='inherit' underline='none'>Resume</Link></Button>
+                            <Button sx={{px: 2}} color='inherit'><Link href='/contact' passHref color='inherit' underline='none'>Contact me</Link></Button>
                         </Box>
                     </Toolbar>
                 </Container>
